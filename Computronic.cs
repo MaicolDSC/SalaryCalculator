c2fe1e0a672123af55b7cdd9ff5bca1fa15f3f57
Maicol Siachoque <136031925+MaicolDSC@users.noreply.github.com>
2024-11-22 16:02:34 -0500

    Add files via upload

---
+++ Computronic.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_2_paso_6
+{
+    internal class Computronic
+    {
+        private int ventas; // Atributo
+
+        public void leerVentas() //Metodos constructor
+        {
+            Console.WriteLine("Ingrese cuantas ventas realizo");
+            ventas=int.Parse(Console.ReadLine());
+
+            int i; // Variable acomulativa
+            for(i = 1;i <= ventas; i++) 
+            {
+                int x;//variable
+                Console.WriteLine("Ingrese el valor de la venta");
+                x = int.Parse(Console.ReadLine());
+                suma = suma + x;
+
+                if (x <= 300000) 
+                {
+                    Console.WriteLine("Esta venta es menor o igual a $300000  ");
+                }
+                else if(x > 300000 && x < 800000) 
+                {
+                    Console.WriteLine("Esta venta es mayor a $300000 y menor a $800000");
+                }
+                else if(x >= 800000) 
+                {
+                    Console.WriteLine("Esta venta es mayor o igual a $800000");
+                }
+
+                //bonificacion
+
+                if (x <= 400000) 
+                {
+                    menoresIguales400 = x * 3 / 100;
+                    Console.WriteLine("Esta venta tiene una bonificacion de $"+ menoresIguales400);
+                }
+                else if(x > 400001 && x < 800000) 
+                {
+                    mayores401Menores800 = x * 5 / 100;
+                    Console.WriteLine("Esta venta tiene una bonificacion de $"+mayores401Menores800);
+                }
+                else if (x > 800000) 
+                {
+                    mayoresIguales800 = x * 10 / 100;
+                    Console.WriteLine("Esta venta tiene una bonificacion de $"+mayoresIguales800);
+                }
+            }
+        }
+        public int SueldoBase() //variable
+        {
+            int sueldoBase;
+            sueldoBase= 500000;
+            return sueldoBase;
+        }
+        int suma = 0;//variable
+
+        public int totalVentas() 
+        { 
+            int totalVentas;
+            totalVentas = suma;
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine("La suma total de las ventas es de $"+ totalVentas);
+            return totalVentas;
+        }
+        double menoresIguales400 = 0; //variable
+        double mayores401Menores800 = 0;//para realiazar la sumatoria de todas las bonificaciones
+        double mayoresIguales800 = 0;
+
+        public double pagoTotal() 
+        {
+            double pagoTotal; // las bonificaciones que se van a recibir + sueldo base
+            pagoTotal = mayoresIguales800 + menoresIguales400 + mayores401Menores800 + 500000;
+            return pagoTotal;
+        }
+        public double totalBonificacion() 
+        {
+            double totalBonificacion;
+            totalBonificacion = mayoresIguales800 + menoresIguales400 + mayores401Menores800;
+            return totalBonificacion;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿/*.
+ * Problema 2: La empresa de tecnología “COMPUTRONIC”, es una empresa 
+líder en venta y distribución de equipos tecnológicos en la ciudad de Bogotá 
+y cuenta en su única sede con un N cantidad de empleados quienes realizan 
+N ventas durante el día. La empresa requiere identificar cuántas de las ventas 
+fueron menores o iguales a $300.000, cuántas ventas fueron mayores a $ 
+300.000 y menores a $800.000 y cuántas fueron mayores o iguales a 
+$800.000 por cada empleado.
+También se necesita conocer monto total por la venta realizada por empleado 
+y las bonificaciones a pagar según la venta realizada. Para el pago de la 
+bonificación es necesario tener en cuenta la siguiente tabla:
+
+Ventas superiores o iguales a $400.000 3%
+Ventas entre $400.001 a $800.000 5%
+Ventas superiores a $800.000 10%
+
+Al finalizar el día, se debe calcular el valor a pagar por empleado, 
+especificando: pago básico y el valor de la bonificación recibida. Tenga en 
+cuenta que el pago básico para todos los empleados es de $500.000
+Desarrolle una solución básica de programación que le permita a la empresa 
+COMPUTRONIC conocer toda la información que es de su interés. Los valores 
+para ingresar deben ser números positivos.
+
+Nombre: Maicol David Siachoque Cubides
+Grupo: 104
+Programa: Ingenieria de sistemas
+Codigo funte: Autoria propia
+
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_2_paso_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------------------------------------------------------------------------------");
+            Console.WriteLine("                                  Bienvenido a COMPUTRONIC                                   ");
+            Console.WriteLine("---------------------------------------------------------------------------------------------");
+            //instanciar Objetos
+            Computronic objComputronics;
+            objComputronics = new Computronic();
+            //intanciar metodos
+            objComputronics.leerVentas();
+            objComputronics.totalVentas();
+
+
+            Console.WriteLine("El sueldo base es de $"+ objComputronics.SueldoBase());
+
+            Console.WriteLine("---------------------------------------------------------------------------------------------");
+
+            Console.WriteLine("El valor total de bonificaciones es de $"+ objComputronics.totalBonificacion());
+
+            Console.WriteLine("---------------------------------------------------------------------------------------------");
+
+            Console.WriteLine("Total a pagar el empleado = sueldo base + bonificacion es de $"+objComputronics.pagoTotal());
+
+            Console.ReadKey();
+
+        }
+    }
+}
